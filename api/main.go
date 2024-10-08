f09e7ba03cfc8afc6ecc456aea3522b46ce939f1
Nafis Anjoom <nafisanjoom@gmail.com>
2024-10-07 22:07:17 -0400

    added a configuration type. Parsing env variables.

--- api/main.go
+++ api/main.go
@@ -4,22 +4,31 @@ import (
 	"chat/chat"
 	"chat/database"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type application struct {
+    config config
     messageService *chat.MessageService
     models *database.Models
 }
 
+type config struct {
+    port int
+    databaseURL string
+    maxDBOpenConns string
+}
+
 func (app *application) serve() {
     srv := &http.Server{
-        Addr: ":3000",
+        Addr: fmt.Sprintf(":%d", app.config.port),
         Handler: app.routes(),
         IdleTimeout: time.Minute,
         ReadTimeout: time.Minute,
@@ -28,21 +37,27 @@ func (app *application) serve() {
 
     go app.messageService.Run()
 
-    log.Printf("starting server on localhost:%d", 3000)
+    log.Printf("starting server on localhost:%d", app.config.port)
     log.Fatal(srv.ListenAndServe())
 }
 
 func main() {
-    postgresUrl := os.Getenv("DATABASE_URL")
+    var config config
 
-    log.Println("database url", postgresUrl)
+    config.databaseURL = os.Getenv("DB_URL")
+    port, err := strconv.ParseInt(os.Getenv("PORT"), 10, 64)
+    if err != nil {
+        log.Fatalln("invalid port")
+    }
+    config.port = int(port)
 
-    dbPool, err := pgxpool.New(context.Background(), postgresUrl)
+    dbPool, err := pgxpool.New(context.Background(), config.databaseURL)
     if err != nil {
         log.Fatalln("unable to open database connection")
     }
 
     app := &application{
+        config: config,
         messageService: chat.NewMessageService(),
         models: database.NewModels(dbPool),
     }

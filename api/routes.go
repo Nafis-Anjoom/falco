d9695acdd9f3b64bcc74f306b29ed9bc098f1ca7
Nafis Anjoom <nafisanjoom@gmail.com>
2024-10-24 02:25:16 -0700

    fixed delete user endpoint route typo

--- api/routes.go
+++ api/routes.go
@@ -20,7 +20,7 @@ func (app *application) routes() http.Handler {
     })
 
     mux.HandleFunc("GET /user/{id}", app.getUserByIdHandler) 
-    mux.HandleFunc("DELTE /user/{id}", app.deleteUserById) 
+    mux.HandleFunc("DELETE /user/{id}", app.deleteUserById) 
     mux.HandleFunc("POST /user", app.createUserHandler) 
 
     return mux

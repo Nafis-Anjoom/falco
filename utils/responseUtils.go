ebd6ce676f95a12240832a11447e919b88dc6481
Nafis Anjoom <nafisanjoom@gmail.com>
2024-10-24 02:26:26 -0700

    added detailed message in the error response from server

--- utils/responseUtils.go
+++ utils/responseUtils.go
@@ -7,16 +7,17 @@ import (
 	"time"
 )
 
-var errorMessageMap = map[int]string {
-    http.StatusNotFound: "the requested resource is not found",
-    http.StatusInternalServerError: "the server encountered an error and could not process your request",
-    http.StatusBadRequest: "invalid request",
+var errorMessageMap = map[int]string{
+	http.StatusNotFound:            "the requested resource is not found",
+	http.StatusInternalServerError: "the server encountered an error and could not process your request",
+	http.StatusBadRequest:          "invalid request",
 }
 
 type ErrorResponse struct {
-    Message   string `json:"message"`
+	Message   string    `json:"message"`
 	TimeStamp time.Time `json:"timestamp"`
-	Path      string `json:"path"`
+	Path      string    `json:"path"`
+	Details   string    `json:"details"`
 }
 
 func WriteJSONResponse(writer http.ResponseWriter, status int, data any) error {
@@ -36,29 +37,29 @@ func WriteJSONResponse(writer http.ResponseWriter, status int, data any) error {
 	return nil
 }
 
-
 func WriteErrorResponse(writer http.ResponseWriter, request *http.Request, statusCode int, err error) {
 	writer.WriteHeader(statusCode)
 
 	errorResponse := ErrorResponse{
-        Message: errorMessageMap[statusCode],
-        TimeStamp: time.Now().UTC(),
-        Path: request.URL.RequestURI(),
-    }
+		Message:   errorMessageMap[statusCode],
+		TimeStamp: time.Now().UTC(),
+		Path:      request.URL.RequestURI(),
+		Details:   err.Error(),
+	}
 
 	json, err := json.MarshalIndent(errorResponse, "", "\t")
 	if err != nil {
-        log.Println(err)
-        writer.WriteHeader(http.StatusInternalServerError)
-        return
+		log.Println(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	json = append(json, '\n')
 	writer.Header().Set("Content-Type", "application/json")
 	_, err = writer.Write(json)
 	if err != nil {
-        log.Println(err)
-        writer.WriteHeader(http.StatusInternalServerError)
-        return
+		log.Println(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }

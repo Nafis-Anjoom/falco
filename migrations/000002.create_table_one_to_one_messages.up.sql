6758351cf5a1e85eb1dde52d5584f3b7b5e0134f
Nafis Anjoom <nafisanjoom@gmail.com>
2024-11-07 05:34:41 -0800

    updated migrations

--- migrations/000002.create_table_one_to_one_messages.up.sql
+++ migrations/000002.create_table_one_to_one_messages.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS public.oneToOneMessages (
-    id BIGSERIAL PRIMARY KEY,
-    senderId INTEGER NOT NULL,
-    receiverId INTEGER NOT NULL,
+    messageId BIGINT PRIMARY KEY,
+    senderId BIGINT NOT NULL,
+    recipientId BIGINT NOT NULL,
     content VARCHAR(255),
     timestamp TIMESTAMP NOT NULL,
     CONSTRAINT fk_sender FOREIGN KEY (senderId) REFERENCES public.users(id),
-    CONSTRAINT fk_receiver FOREIGN KEY (receiverId) REFERENCES public.users(id)
+    CONSTRAINT fk_recipient FOREIGN KEY (recipientId) REFERENCES public.users(id)
 );

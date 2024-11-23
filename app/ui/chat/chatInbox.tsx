cf5913845bb0fff40135f4c398bb4ad7d0646904
Nafis Anjoom <nafisanjoom@gmail.com>
2024-11-22 21:42:02 -0800

    chat inbox padding fix

--- app/ui/chat/chatInbox.tsx
+++ app/ui/chat/chatInbox.tsx
@@ -2,7 +2,7 @@ import ChatCard from "./chatcard";
 
 export default function ChatInbox() {
     return(
-        <div className="flex h-full flex-col bg-zinc-800 py-4 pl-4 w-96">
+        <div className="flex h-full flex-col bg-zinc-800 pt-4 pl-4 w-96">
             <div className="bg-green text-xl font-bold">Chats</div>
             <div className="flex mt-3 pr-4">
                 <input className="w-full h-8 rounded-md px-2 text-black" type="text" placeholder="Search chat"/>

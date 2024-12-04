537e9f760defddbbdd6b9ccde4d3aa0cdcfc3376
Nafis Anjoom <nafisanjoom@gmail.com>
2024-12-03 23:26:36 -0800

    added log out logic

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -1,11 +1,35 @@
+"use client";
+
 import { UserCircleIcon, ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
+    const router = useRouter();
+
+    async function handleLogout() {
+        try {
+            const response = await fetch("http://localhost:3000/logout", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+            });
+
+            if (response.ok) {
+                router.push("/login");
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return(
-        // <div className="flex h-full flex-col justify-between py-4 px-1.5 border-r-[1px] border-r-blue-500">
         <div className="flex h-full flex-col justify-between py-4 px-1.5 bg-blue-500">
-            <UserCircleIcon className="h-8 w-8" />
-            <ArrowLeftStartOnRectangleIcon className="h-8 w-8" />
+            <div className="p-1">
+                <UserCircleIcon className="h-8 w-8" />
+            </div>
+            <button onClick={() => handleLogout()} className="p-1 hover:bg-blue-400 rounded-lg">
+                <ArrowLeftStartOnRectangleIcon className="h-8 w-8" />
+            </button>
         </div>
     );
 }

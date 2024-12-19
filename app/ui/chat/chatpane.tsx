0047830e65a28bfe4b4edf75e6044075f9cd73d7
Nafis Anjoom <nafisanjoom@gmail.com>
2024-12-18 22:45:14 -0800

    put the comment in the right place

--- app/ui/chat/chatpane.tsx
+++ app/ui/chat/chatpane.tsx
@@ -14,6 +14,7 @@ export default function ChatPane({ contact, messages, sendMessage }: ChatPanePro
   const chatPaneRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // give the browser a moment to insert the new message before scrolling
     setTimeout(() => {
       if (chatPaneRef.current) {
         chatPaneRef.current.scrollTop = chatPaneRef.current.scrollHeight;
@@ -40,7 +41,6 @@ export default function ChatPane({ contact, messages, sendMessage }: ChatPanePro
 
     sendMessage(content);
     textareaRef.current.value = "";
-    // give the browser a moment to insert the new message before scrolling
   }
 
   return (
@@ -60,9 +60,6 @@ export default function ChatPane({ contact, messages, sendMessage }: ChatPanePro
           );
           return output;
         })}
-        {/* <div className="flex w-full mt-2 justify-end">
-            <div className="max-w-96 text-zinc-400 text-xs" >seen at 12:54pm</div>
-        </div> */}
       </div>
       <div className="flex flex-shrink-0 flex-grow-0 w-full border-t-2 border-zinc-800">
         <div className="flex-grow ">
